d46d794e64b4a0170c76c7f1edfc5d784b4b8e05
ArijitDutta96395 <132121869+ArijitDutta96395@users.noreply.github.com>
2025-08-22 03:49:33 +0530

    Updated portfolio with new changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
             Arijit Dutta &nbsp;
-            <span className='sm:block hidden'> | ECE Engineer</span>
+            <span className='sm:block hidden'> | ECE Undergrad</span>
           </p>
         </Link>
 
